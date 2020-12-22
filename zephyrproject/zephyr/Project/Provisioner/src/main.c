ca20a9e56fe7aa70aa5081b5cdf65db96a92c22f
Boss Bibentyo <bibenb1@users.noreply.github.com>
2020-12-22 13:55:41 -0500

    adding ethernet link board connectivity. including DHCP configuration

--- zephyrproject/zephyr/Project/Provisioner/src/main.c
+++ zephyrproject/zephyr/Project/Provisioner/src/main.c
@@ -1,7 +1,16 @@
 #include <zephyr.h>
 #include "mesh.h"
 
+#ifdef CONFIG_NET_MGMT_EVENT
+	#include "dhcp_config.h"
+#else
+	void initialize_dhcp() {
+		printk("Network configuration not enabled\n");
+	}
+#endif
+
 void main(void)
 {
+	initialize_dhcp();
 	mesh_start();
 }
